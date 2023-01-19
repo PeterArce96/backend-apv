c786fbf0a3ccbe7b99d6101fcb2eb6e34adcd6d9
PeterArce96 <parce0496@gmail.com>
2023-01-19 16:45:18 -0500

    apv - Identificando si un usuario existe para Autenticarlo

--- backend/controllers/veterinarioController.js
+++ backend/controllers/veterinarioController.js
@@ -49,9 +49,20 @@ const confirmar = async (req, res) => {
     }
 }
 
-const autenticar = (req, res) => {
-    console.log(req.body);
-    res.json({ msg: "autenticando" });
+const autenticar = async (req, res) => {
+    const { email } = req.body
+
+    // Comprobar si el usuario existe
+    const usuario = await Veterinario.findOne({email});
+
+    if (!usuario) {
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({msg: error.message});
+    }
+
+    // Comprobar si el usuario está confirmado
+    
+    
 };
 
 
