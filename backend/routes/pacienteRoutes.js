4687de3d5a06c740f2233c97ca3fd0d971d28e11
PeterArce96 <parce0496@gmail.com>
2023-01-24 17:23:36 -0500

    apv - Obtener un Paciente en específico

--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const router = express.Router();
-import { agregarPaciente, obtenerPacientes } from '../controllers/pacienteController.js'
+import { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente } from '../controllers/pacienteController.js'
 
 import checkAuth from '../middleware/authMiddleware.js';
 
@@ -9,5 +9,11 @@ router
     .route('/')
     .post(checkAuth, agregarPaciente)
     .get(checkAuth, obtenerPacientes);
+
+router
+    .route('/:id')
+    .get(checkAuth, obtenerPaciente)
+    .put(checkAuth, actualizarPaciente)
+    .delete(checkAuth, eliminarPaciente)
 
 export default router;
