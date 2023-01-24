2411b60053a0c035fe505802361bc38667a952ea
PeterArce96 <parce0496@gmail.com>
2023-01-24 16:56:06 -0500

    apv - Almacenando el veterinario en el registro del Paciente

--- backend/routes/pacienteRoutes.js
+++ backend/routes/pacienteRoutes.js
@@ -3,6 +3,8 @@ import express from 'express'
 const router = express.Router();
 import { agregarPaciente, obtenerPacientes } from '../controllers/pacienteController.js'
 
-router.route('/').post(agregarPaciente).get(obtenerPacientes);
+import checkAuth from '../middleware/authMiddleware.js';
+
+router.route('/').post(checkAuth, agregarPaciente).get(obtenerPacientes);
 
 export default router;
