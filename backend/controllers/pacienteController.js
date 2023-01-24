333217f9e36423048e26d3432f2c73e492d4b071
PeterArce96 <parce0496@gmail.com>
2023-01-24 17:39:37 -0500

    apv - Eliminar Paciente

--- backend/controllers/pacienteController.js
+++ backend/controllers/pacienteController.js
@@ -59,7 +59,23 @@ const actualizarPaciente = async (req, res) => {
 
 }
 const eliminarPaciente = async (req, res) => {
+    const { id } = req.params;
+    const paciente = await Paciente.findById(id);
 
+    if (!paciente) {
+        res.status(404).json({msg: "No encontrado"});
+    }
+
+    if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+        return res.json({msg: 'Accion no válida'});
+    }
+
+    try {
+        await paciente.deleteOne()
+        res.json({msg: 'Paciente Eliminar'});
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 export { agregarPaciente, obtenerPacientes, obtenerPaciente, actualizarPaciente, eliminarPaciente };
