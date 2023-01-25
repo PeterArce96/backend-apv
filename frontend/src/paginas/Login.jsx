4e7ed87537a7a35154ff4069a27693765007ddf5
PeterArce96 <parce0496@gmail.com>
2023-01-25 17:11:34 -0500

    apv - Creando el formulario para el Login

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -9,7 +9,32 @@ const Login = () => {
                 </h1>
             </div>
             <div>
-                2
+                <form action="">
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Email
+                        </label>
+                        <input
+                            type="email"
+                            placeholder="Email de Registro"
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Password
+                        </label>
+                        <input
+                            type="password"
+                            placeholder="Tu Password"
+                            className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        />
+                    </div>
+                    <input type="submit" 
+                        value="Iniciar Sesión"
+                        className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                    />
+                </form>
             </div>
             
         </>
