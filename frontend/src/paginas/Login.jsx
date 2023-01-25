8afae4d19715d276952096b76c8fc42b96671a4c
PeterArce96 <parce0496@gmail.com>
2023-01-25 17:28:38 -0500

    apv - Creando la página de Registro de Usuario

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
                 <h1 className="text-indigo-600 font-black text-6xl">Inicia Sesión y Administra tus <span className="text-black">Pacientes</span>
                 </h1>
             </div>
-            <div>
+            <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
                 <form action="">
                     <div className="my-5">
                         <label className="uppercase text-gray-600 block text-xl font-bold">
