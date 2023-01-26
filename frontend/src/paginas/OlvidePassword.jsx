99fb9822a8cb7c9b5f9d7e413772391b2c864b35
PeterArce96 <parce0496@gmail.com>
2023-01-26 18:06:35 -0500

    apv - Generando token y validación del token único para cambiar el password

--- frontend/src/paginas/OlvidePassword.jsx
+++ frontend/src/paginas/OlvidePassword.jsx
@@ -1,6 +1,34 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 
 const OlvidePassword = () => {
+    const [email, setEmail] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const handleSubmit = async e => {
+        e.preventDefault()
+
+        if(email === '' || email.length < 6){
+            setAlerta({msg: "El email es obligatorio", error: true})
+            return
+        }
+        try {
+            const { data } = await clienteAxios.post('/veterinarios/olvide-password', {email})
+            console.log(data)
+            
+            setAlerta({msg: data.msg})
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error:true
+            })
+        }
+    }
+
+    const { msg } = alerta
+
     return (
 
         <>
@@ -10,7 +38,13 @@ const OlvidePassword = () => {
             </div>
 
             <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-                <form action="">
+                {msg && <Alerta
+                    alerta={alerta}
+                />}
+
+                <form 
+                    onSubmit={handleSubmit}
+                >
                     
                     <div className="my-5">
                         <label className="uppercase text-gray-600 block text-xl font-bold">
@@ -20,6 +54,8 @@ const OlvidePassword = () => {
                             type="email"
                             placeholder="Email de Registro"
                             className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                            value={email}
+                            onChange= {e => setEmail(e.target.value)}
                         />
                     </div>
                     <input type="submit" 
