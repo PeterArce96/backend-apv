412909e6031e54228112aa53f78dca5ca6e79426
PeterArce96 <parce0496@gmail.com>
2023-01-27 16:49:58 -0500

    apv - Creando un custom hook y accediendo a los datos del context

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -5,10 +5,14 @@ const AuthContext = createContext()
 const AuthProvider = ({children}) => {
 
     const [ auth, setAuth ] = useState({})
-    
+
 
     return(
-        <AuthContext.Provider>
+        <AuthContext.Provider
+            value={{
+                auth, setAuth
+            }}
+        >
 
             {children}
         </AuthContext.Provider>
--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
 
 const Login = () => {
+
     return (
         <>
             
