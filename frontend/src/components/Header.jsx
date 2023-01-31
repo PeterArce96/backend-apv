23a8a57b9c88f195eb1d27b3d8497df7215abed8
PeterArce96 <parce0496@gmail.com>
2023-01-31 17:31:32 -0500

    apv - Creando los componentes para Editar Perfil y Cambiar contraseña

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
 
                 <nav className="flex flex-col lg:flex-row gap-4 mt-5 lg:mt-0 items-center">
                     <Link to ="/admin" className="text-white text-sm uppercase font-bold">Pacientes</Link>
-                    <Link to ="/perfil" className="text-white text-sm uppercase font-bold">Perfil</Link>
+                    <Link to ="/admin/perfil" className="text-white text-sm uppercase font-bold">Perfil</Link>
 
                     <button
                         type="button"
